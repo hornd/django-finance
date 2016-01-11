aa99473750a42b34eb018efb72f82ce732347606
hornd <douglas.j.horn@gmail.com>
2016-01-10 17:10:30 -0800

    Adding model.

--- goals/finance_goal/models.py
+++ goals/finance_goal/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Goal(models.Model):
+    current_amount = models.IntegerField(default=0)
+    goal_amount = models.IntegerField()
+    end_date = models.DateTimeField('Due')
--- goals/goals/settings.py
+++ goals/goals/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'finance_goal.apps.Finance_goalConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
