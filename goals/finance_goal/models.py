64f1111decac8e9727c4b2eb28e5a41a11f33702
Douglas Horn <douglas.j.horn@gmail.com>
2016-01-11 01:30:01 +0000

    Added admin...

--- goals/finance_goal/models.py
+++ goals/finance_goal/models.py
@@ -4,3 +4,6 @@ class Goal(models.Model):
     current_amount = models.IntegerField(default=0)
     goal_amount = models.IntegerField()
     end_date = models.DateTimeField('Due')
+
+    def __str__(self):
+        return str(self.current_amount) + ' / ' + str(self.goal_amount) + ' : ' + str(self.end_date)
