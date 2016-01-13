59ec068b96a5f6bb209ffbb164b761b615aae162
Douglas Horn <douglas.j.horn@gmail.com>
2016-01-13 04:49:21 +0000

    Script, styling.

--- goals/finance_goal/static/finance_goal/script.js
+++ goals/finance_goal/static/finance_goal/script.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
-        $("[class^=finance_goal]").each(function() {
-                $(this).mouseover(function() {
-                        $(this).addClass("highlight");
-                    });
+  $("[class^=finance_goal]").each(function() {
+    $(this).mouseover(function() {
+      $(this).addClass("highlight");
+    });
 
-                $(this).mouseout(function() {
-                        $(this).removeClass("highlight");
-                    });
-                
-            });
+    $(this).mouseout(function() {
+      $(this).removeClass("highlight");
+    });
+
+    var end_val = $(this).find(".goal_end").html();
+    $(this).find(".goal_progress").css("width", end_val);
+  });
 });
