74dea86b4c1229efd801e8bd6c2d2b3388befef3
Douglas Horn <douglas.j.horn@gmail.com>
2016-01-14 03:55:42 +0000

    Changed goal model to decimal, added transaction model.

--- goals/finance_goal/models.py
+++ goals/finance_goal/models.py
@@ -2,9 +2,17 @@ from django.db import models
 
 class Goal(models.Model):
     description = models.CharField(max_length=200, default='')
-    current_amount = models.IntegerField(default=0)
-    goal_amount = models.IntegerField()
-    end_date = models.DateTimeField('Due')
+    current_amount = models.DecimalField(default=0, max_digits=8, decimal_places=2)
+    goal_amount = models.DecimalField(default=0, max_digits=8, decimal_places=2)
+    end_date = models.DateField('Due')
 
     def __str__(self):
         return str(self.current_amount) + ' / ' + str(self.goal_amount) + ' : ' + str(self.end_date)
+
+class Transaction(models.Model):
+    goal = models.ForeignKey(Goal, on_delete=models.CASCADE)
+    amount = models.DecimalField(default=0, max_digits=8, decimal_places=2)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.amount
