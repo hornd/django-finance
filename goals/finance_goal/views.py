fcb487d796ea02f88ede00524e6c52b637f4a27d
Douglas Horn <doug@douglasjhorn.com>
2016-01-15 04:28:00 +0000

    Stub detail page implementation

--- goals/finance_goal/views.py
+++ goals/finance_goal/views.py
@@ -6,3 +6,9 @@ def index(request):
     goal_list = Goal.objects.all()
     context = {'goal_list': goal_list}
     return render(request, 'finance_goal/index.html', context)
+
+def detail(request, goal_id):
+    goal = Goal.objects.get(pk=goal_id)
+    context = {'goal': goal}
+    return render(request, 'finance_goal/detail.html', context)
+    
